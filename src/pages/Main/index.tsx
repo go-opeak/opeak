67664d05c278aa397c3d3078a20a4f7c69807adf
kimgoat <kimgoatlove@gmail.com>
2024-09-10 20:01:33 +0900

    test: theme 적용 테스트

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,13 @@
+import styled from "styled-components";
+
 export const Main = () => {
   return (
-    <>
+    <Wrapper>
       <div>Main Page</div>
-    </>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  background-color: ${(props) => props.theme.colors.primary.dark};
+`;
